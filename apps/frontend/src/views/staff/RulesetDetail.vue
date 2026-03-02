71d7ecd30dfaabeebada927b9cc6f3a57c68f01a
David Soff <david@funda.nl>
2026-03-02 10:03:52 +0100

    fix: address PR review comments
    - Add missing staff RulesetDetail view used by ruleset create/detail routes

---
+++ apps/frontend/src/views/staff/RulesetDetail.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const rulesetId = computed(() => route.params.rulesetId)
+const isCreateMode = computed(() => !rulesetId.value)
+</script>
+
+<template>
+  <div class="staff-ruleset-detail">
+    <h1>{{ isCreateMode ? 'Create Ruleset' : 'Ruleset Detail' }}</h1>
+    <p v-if="isCreateMode">Ruleset creation form will be implemented in follow-up work.</p>
+    <p v-else>Ruleset ID: {{ rulesetId }}</p>
+  </div>
+</template>
+
+<style scoped>
+.staff-ruleset-detail {
+  padding: var(--rd-space-4);
+}
+
+h1 {
+  margin: 0 0 var(--rd-space-3) 0;
+  font-size: var(--rd-text-xl);
+  font-weight: var(--rd-weight-semibold);
+}
+
+p {
+  margin: 0;
+}
+</style>
